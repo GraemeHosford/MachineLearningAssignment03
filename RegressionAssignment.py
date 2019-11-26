aa812cbc0e068375a59a0ee1a0f219dfb789544c
Graeme Hosford <graemehosford@gmail.com>
2019-11-26 13:56:36 +0000

    Worked on tasks 2 - 4

--- RegressionAssignment.py
+++ RegressionAssignment.py
@@ -25,22 +25,79 @@ def task1():
     for feature in features:
         print("The number of datapoints in this feature is", len(feature))
 
-    useable_features = list(filter(lambda x: len(x) > 800, features))
-    useable_targets = list(filter(lambda x: len(x) > 800, target))
+    useable_features = list(filter(lambda f: len(f) > 800, features))
+    useable_targets = list(filter(lambda t: len(t) > 800, target))
 
     print("\nAfter filtering the feature subsets there is", len(useable_features), "left to be used\n")
 
     for feature in useable_features:
         print("The number of datapoints in these features with more than 800 datapoints is", len(feature))
 
-    return useable_features, useable_targets
+    feature_dataframe = useable_features[0]
+    target_dataframe = useable_targets[0]
+
+    for x in range(1, len(useable_features)):
+        feature_dataframe.append(useable_features[x])
+
+    for x in range(1, len(useable_targets)):
+        target_dataframe.append(useable_targets[x])
+
+    return feature_dataframe.to_numpy(), target_dataframe.to_numpy()
 
 
-def task2(features: pd.DataFrame, coff: np.array, deg: int):
-    print()
+def num_coefficients(deg: int) -> int:
+    t = 0
+    for n in range(deg + 1):
+        for i in range(n + 1):
+            for j in range(n + 1):
+                if i + j == n:
+                    t += 1
+    return t
+
+
+def task2(data: np.array, p: np.array, deg: int) -> np.array:
+    result = np.zeros(data.shape[0])
+    k = 0
+    for n in range(deg + 1):
+        for i in range(n + 1):
+            result += p[k] * (data[:, 0] ** i) * (data[:, 1] ** (n - i))
+            k += 1
+    return result
+
+
+def task3(deg: int, data: np.array, p0: np.array):
+    f0 = task2(data, p0, deg)
+    j = np.zeros((len(f0), len(p0)))
+    epsilon = 1e-6
+    for i in range(len(p0)):
+        p0[i] += epsilon
+        fi = task2(data, p0, deg)
+        p0[i] -= epsilon
+        di = (fi - f0) / epsilon
+        j[:, i] = di
+    return f0, j
+
+
+def task4(y, f0, j):
+    l = 1e-2
+    N = np.matmul(j.T, j) + l * np.eye(j.shape[1])
+    r = y - f0
+    n = np.matmul(j.T, r)
+    dp = np.linalg.solve(N, n)
+    return dp
 
 
 def main():
     features, target = task1()
+
+    max_iter = 10
+
+    for deg in range(5):
+        p0 = np.zeros(num_coefficients(deg + 1))
+        for i in range(max_iter):
+            f0, j = task3(deg, features, p0)
+            dp = task4(target, f0, j)
+            p0 += dp
+
 
 main()
